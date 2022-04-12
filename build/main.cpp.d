5faaed94ae6802aadfc8c6d49547ed674b172f99
zty001222 <1900013052@pku.edu.cn>
2022-04-12 05:58:50 -0700

    finishlv6koopa

--- build/main.cpp.d
+++
@@ -1,9 +0,0 @@
-/root/compiler/build/main.cpp.o: /root/compiler/src/main.cpp \
-  /opt/include/koopa.h /root/compiler/src/myast.h \
-  /root/compiler/src/myparser.h
-
-/opt/include/koopa.h:
-
-/root/compiler/src/myast.h:
-
-/root/compiler/src/myparser.h:
--- build/sysy.lex.cpp.d
+++
@@ -1,6 +0,0 @@
-/root/compiler/build/sysy.lex.cpp.o: /root/compiler/build/sysy.lex.cpp \
-  /root/compiler/build/sysy.tab.hpp /root/compiler/src/myast.h
-
-/root/compiler/build/sysy.tab.hpp:
-
-/root/compiler/src/myast.h:
